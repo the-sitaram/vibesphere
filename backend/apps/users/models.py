0d0f96a71dae9ee13e490615ad38dab8d1b2333a
the-sitaram <yraj38377@gmail.com>
2025-07-30 12:28:27 +0530

    Update models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -40,38 +40,38 @@ class Follow(models.Model):
     def __str__(self):
         return f"{self.follower.username} follows {self.following.username}"
 
-# backend/apps/users/serializers.py
-from rest_framework import serializers
-from django.contrib.auth import get_user_model
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+# # backend/apps/users/serializers.py
+# from rest_framework import serializers
+# from django.contrib.auth import get_user_model
+# from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 
-User = get_user_model()
+# User = get_user_model()
 
-class UserCreateSerializer(BaseUserCreateSerializer):
-    class Meta(BaseUserCreateSerializer.Meta):
-        fields = ('id', 'username', 'email', 'password')
+# class UserCreateSerializer(BaseUserCreateSerializer):
+#     class Meta(BaseUserCreateSerializer.Meta):
+#         fields = ('id', 'username', 'email', 'password')
 
-class UserSerializer(serializers.ModelSerializer):
-    is_following = serializers.SerializerMethodField()
-    is_followed_by = serializers.SerializerMethodField()
+# class UserSerializer(serializers.ModelSerializer):
+#     is_following = serializers.SerializerMethodField()
+#     is_followed_by = serializers.SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = (
-            'id', 'username', 'email', 'bio', 'profile_picture',
-            'is_private', 'followers_count', 'following_count',
-            'posts_count', 'website', 'is_following', 'is_followed_by'
-        )
-        read_only_fields = ('followers_count', 'following_count', 'posts_count')
+#     class Meta:
+#         model = User
+#         fields = (
+#             'id', 'username', 'email', 'bio', 'profile_picture',
+#             'is_private', 'followers_count', 'following_count',
+#             'posts_count', 'website', 'is_following', 'is_followed_by'
+#         )
+#         read_only_fields = ('followers_count', 'following_count', 'posts_count')
 
-    def get_is_following(self, obj):
-        request = self.context.get('request')
-        if request and request.user.is_authenticated:
-            return obj.followers.filter(follower=request.user).exists()
-        return False
+#     def get_is_following(self, obj):
+#         request = self.context.get('request')
+#         if request and request.user.is_authenticated:
+#             return obj.followers.filter(follower=request.user).exists()
+#         return False
 
-    def get_is_followed_by(self, obj):
-        request = self.context.get('request')
-        if request and request.user.is_authenticated:
-            return obj.following.filter(following=request.user).exists()
-        return False
+#     def get_is_followed_by(self, obj):
+#         request = self.context.get('request')
+#         if request and request.user.is_authenticated:
+#             return obj.following.filter(following=request.user).exists()
+#         return False
